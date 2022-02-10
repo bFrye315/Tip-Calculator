77d0385c08fe2c1a3a63c3208023e12b4e6aaa17
Brandon <bhf315@gmail.com>
2022-02-09 23:38:41 -0600

    updated to do nothing, not sure if correct way

--- app/src/main/java/com/example/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -20,12 +20,22 @@ public class MainActivity extends AppCompatActivity {
         binding.calculateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                float totalBill = Float.valueOf(String.valueOf(binding.tBill.getText()));
-                int percent = Integer.valueOf(String.valueOf(binding.tipPercent.getText()));
-                int peopleNum = Integer.valueOf(String.valueOf(binding.numOfPeople.getText()));
-                float totalCal = ((((float)percent / 100) + 1) * totalBill) / peopleNum ;
-                String total = String.format("%.2f", totalCal);
-                binding.amountText.setText("Total Per Person: $" + total);
+                String totalBillS, percentS, peopleNumS;
+                totalBillS = String.valueOf(binding.tBill.getText());
+                percentS = String.valueOf(binding.tipPercent.getText());
+                peopleNumS = String.valueOf(binding.numOfPeople.getText());
+                if (totalBillS.isEmpty() || peopleNumS.isEmpty() || peopleNumS.isEmpty()){
+                    // do nothing
+                }
+                else{
+                    float totalBill = Float.parseFloat(totalBillS);
+                    int percent = Integer.parseInt(percentS);
+                    int peopleNum = Integer.parseInt(peopleNumS);
+                    float totalCal = ((((float)percent / 100) + 1) * totalBill) / peopleNum ;
+                    String total = String.format("%.2f", totalCal);
+                    binding.amountText.setText(new StringBuilder().append(getString(R.string.tPerPerson)).append(total).toString());
+                }
+
 
             }
         });
